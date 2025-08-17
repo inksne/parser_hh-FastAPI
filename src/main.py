f0e72538406328729a968fb0685d890ebb31827e
inksne <vitalijjapr@gmail.com>
2025-08-17 11:00:11 +0300

    добавление: добавлено подключение роутеров и CORS

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+
+from database.database import create_db_and_tables
+
+from auth import router as auth_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+
+app = FastAPI(title='parser_hh')
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router)
