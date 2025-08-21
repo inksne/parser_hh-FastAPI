653c211c22a0538dad89bd0eea4c2f8becf791c1
inksne <vitalijjapr@gmail.com>
2025-08-21 20:53:28 +0300

    обновление: добавлены новые поля

---
+++ src/basemodels.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class GetVacanciesModel(BaseModel):
+    page: Optional[int] = 0
+    per_page: Optional[int] = 20
+    experience: Optional[str] = None
+    employment_form: Optional[str] = None
+    work_format: Optional[str] = None
+    schedule: Optional[str] = None
+    area: Optional[str] = None
+    salary: Optional[str] = None
+    currency: Optional[str] = None
+    metro: Optional[str] = None
+    education: Optional[str] = None
+    no_magic: Optional[bool] = True
+    premium: Optional[bool] = False
+    responses_count_enabled: Optional[bool] = True
