66ca5db6ff7e3055f5616214fad517b0a721aee6
inksne <vitalijjapr@gmail.com>
2025-08-22 21:47:15 +0300

    обновление: изменен lifespan

--- src/main.py
+++ src/main.py
@@ -2,20 +2,32 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
+import logging
+
 from database.database import create_db_and_tables
+from parse_hh import load_area_resolver_from_file
+from config import configure_logging
 
 from auth import router as auth_router
 from templates import router as templates_router
-from parse_hh.parse_hh import router as parse_hh_router
+from parse_hh import router as parse_hh_router
+
+
+
+configure_logging()
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    app.state.area_resolver = load_area_resolver_from_file("parse_hh/areas.json")
+    logger.info('преобразователь areas в id загружен')
+
     await create_db_and_tables()
     yield
 
 
-app = FastAPI(title='parser_hh')
+app = FastAPI(title='parser_hh', lifespan=lifespan)
 
 
 app.add_middleware(
